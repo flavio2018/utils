d051696ce972c030b8cb7a3ff4b619bf5318fa64
Flavio <flavio.petruzzellis@gmail.com>
2022-10-14 16:07:25 +0200

    Refactor get_hidden_mask

--- rnn_utils.py
+++ rnn_utils.py
@@ -41,10 +41,11 @@ def get_mask(lens, device):
 	return torch.tensor([1 if l > 0 else 0 for l in lens], device=device)
 
 
-def get_hidden_mask(mask_1d, h_size, device=device):
+def get_hidden_mask(lens, h_size, device=device):
 	"""Given a mask for a position in a batch of sequences, produce a mask for
 	the batch of hidden states of a recurrent network to be used to process the
 	elements in the batch in the current position."""
+	mask_1d = get_mask(lens, device)
 	return torch.concat(
 		[torch.zeros((1, h_size), device=device) if m == 0 else torch.ones((1, h_size), device=device) for m in mask_1d],
 		dim=0)
